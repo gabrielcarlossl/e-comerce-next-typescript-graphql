338b9d6af64fdea20164380dd9823d3b67f04522
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2023-08-10 21:52:07 -0300

    feat: added header

---
+++ src/components/CartControl.tsx
@@ -0,0 +1,31 @@
+import { useLocalStorage } from '@/hooks/useLocalStore';
+import { CartIcon } from '@/icons/CartIcon';
+import { styled } from 'styled-components';
+
+const CartCount = styled.span`
+    width: 17px;
+    height: 17px;
+    background-color: var(--delete-color);
+    color: #fff;
+    left: 10px;
+    top: 50%;
+    margin-left: -10px;
+    border-radius: 100%;
+    padding: 0px 5px;
+    font-size: 10px;
+`
+
+const CartContainer = styled.div`
+    position: relative;
+`
+
+export function CartControl(){
+    const { value } = useLocalStorage('cart-items', [])
+
+    return (
+        <CartContainer>
+            <CartIcon/>
+            {value.length && <CartCount>{value.length}</CartCount>}
+        </CartContainer>
+    )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import React from 'react'
+import { styled } from 'styled-components'
+
+import { Saira_Stencil_One } from 'next/font/google'
+import { SearchInputWithIcon } from './SearchInput'
+import { CartControl } from './CartControl'
+
+const saira_stencil_one = Saira_Stencil_One({ subsets: ['latin'], weight: ['400'] })
+
+interface HeaderProps { }
+
+const CustomHeader = styled.header`
+    
+    display: flex;
+    align-items: center;
+    justify-content:space-between;
+    padding: 20px 160px;
+    > div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 4px;
+    }
+`
+const Logo = styled.a`
+    color: var(--logo-color);
+    font-size: 40px;
+    line-height: 150%;
+
+`
+const Header = (props: HeaderProps) => {
+    return (
+        <CustomHeader>
+            <Logo className={saira_stencil_one.className}>E-Commerce</Logo>
+            <div>
+                <SearchInputWithIcon placeholder='Procurando por algo específico?' />
+                <CartControl />
+            </div>
+        </CustomHeader>
+    )
+}
+
+export default Header
