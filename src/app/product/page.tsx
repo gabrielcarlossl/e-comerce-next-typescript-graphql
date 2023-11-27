b68b5fa6d3b65631f57b6d454e5390de15a016aa
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2023-11-27 19:35:42 -0300

    feat: added cart counting function

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -111,6 +111,31 @@ const ProductInfo = styled.div`
 
 const Product = ({ searchParams }: { searchParams: { id: string } }) => {
   const { data } = useProduct(searchParams.id)
+  const handleAddToCart = () => {
+    let cartItems = localStorage.getItem('cart-items')
+    if (cartItems){
+      let cartItemsArray = JSON.parse(cartItems) 
+      
+      let existingProductIndex = cartItemsArray.findIndex((item: {id: string}) => item.id === searchParams.id)
+      if(existingProductIndex != -1){
+        cartItemsArray[existingProductIndex].quantity += 1
+      } else {
+        cartItemsArray.push({...data, quantity: 1, id:searchParams.id})
+      }
+      localStorage.setItem('cart-items', JSON.stringify(cartItemsArray))
+    } else{
+      const newCart = [
+        {
+          ...data,
+          id:searchParams.id,
+          quantity: 1 
+        }
+      ]
+      localStorage.setItem('cart-items', JSON.stringify(newCart))
+    }
+    window.location.reload()
+  }
+
   return (
     <DefaultPageLayout>
       <Container>
@@ -128,7 +153,7 @@ const Product = ({ searchParams }: { searchParams: { id: string } }) => {
                 <p>{data?.description}</p>
               </div>
             </ProductInfo>
-            <button>
+            <button onClick={handleAddToCart}>
               <BagIcon/>
               Adicionar ao carrinho
             </button>
